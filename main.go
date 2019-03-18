7598fc68e86ed94e8cb2e9a3e1a3cd0f61865e0f
romber <romber2001@163.com>
2019-03-18 22:24:00 +0800

    init commit

--- main.go
+++ main.go
@@ -1,81 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/pingcap/parser"
-	"github.com/pingcap/parser/ast"
-	"reflect"
+	_ "github.com/pingcap/tidb/types/parser_driver"
+	"os"
 )
 
-import _ "github.com/pingcap/tidb/types/parser_driver"
-
-func elementExistsInSlice(str string, slice []string) bool {
-	for i := range slice {
-		if slice[i] == str {
-			return true
-		}
-	}
-
-	return false
-}
-
-type visitor struct {
-	tableList []string
-}
-
-func (v *visitor) Enter(in ast.Node) (out ast.Node, skipChildren bool) {
-	//fmt.Printf("%T\n", in)
-	if reflect.TypeOf(in).String() == "*ast.TableName" {
-		tableName := in.(*ast.TableName).Name.L
-		if !elementExistsInSlice(tableName, v.tableList) {
-			v.tableList = append(v.tableList, tableName)
-		}
-	}
-	return in, false
-}
-
-func (v *visitor) Leave(in ast.Node) (out ast.Node, ok bool) {
-	return in, true
-}
-
 func main() {
+	var (
+		sqlMap  map[string]interface{}
+		sqlText string
+	)
 
-	sql := "SELECT /*+ TIDB_SMJ(employees) */ emp_no, first_name, last_name " +
-		"FROM employees emp USE INDEX (last_name) " +
-		"" +
-		"where last_name='Aamodt' and gender='F' and birth_date > '1960-01-01'"
+	f := MyFlag{}
+	f.Init()
 
-	sql = `select t1.id, t2.name from (select * from tb01 group by name order by id desc limit 10) t1 , tb02 t2 where t1.id=t2.id
-    union all
-    select t3.id, t4.name from tb03 t3, tb02 t4 where t4.id=t3.id and t3.name = "from table01 to tb02" group by t3.name;`
+	parser := SqlParser{}
 
-	sql = `SELECT a.time_updated_server/1000,
-content,
-nick,
-name
-FROM table1 a
-JOIN table2 b ON a.sender_id = b.user_id
-JOIN table3 c ON a.channel_id = c.channel_id
-JOIN table4 d ON c.store_id = d.store_id
-WHERE sender_id NOT IN
-  (SELECT user_id
-   FROM table5
-   WHERE store_id IN ('agent_store:1',
-                                     'ask:1'))
-   GROUP BY 1,2,3,4
-   HAVING sum(1) > 500
-   ORDER BY 1 ASC;`
-	sqlParser := parser.New()
-	stmtNodes, warn, err := sqlParser.Parse(sql, "", "")
-	if warn != nil || err != nil {
-		fmt.Printf("parse warn:\n%v\n%s", warn, sql)
-		fmt.Printf("parse error:\n%v\n%s", err, sql)
-		return
-	}
-	for _, stmtNode := range stmtNodes {
-		v := visitor{}
-		//tableList := []string{}
-		stmtNode.Accept(&v)
-		fmt.Println(v.tableList)
+	if format == "text" {
+		sqlText = sql
+		if tables, warns, err := parser.GetTables(sqlText); warns == nil && err == nil {
+			fmt.Println(tables)
+		} else {
+			fmt.Printf("parse warn:\n%v\nparse error:\n%v\n", warns, err)
+			os.Exit(1)
+		}
+	} else if format == "json" {
+		if err := json.Unmarshal([]byte(sql), &sqlMap); err == nil {
+			sqlText = sqlMap["sqlText"].(string)
+			if tables, warns, err := parser.GetTables(sqlText); warns == nil && err == nil {
+				tableMap := make(map[string]interface{})
+				tableMap["tables"] = tables
+
+				if tableMapBytes, err := json.Marshal(tableMap); err == nil {
+					fmt.Println(string(tableMapBytes))
+				} else {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+			} else {
+				fmt.Printf("parse warn:\n%v\nparse error:\n%v\n", warns, err)
+				os.Exit(1)
+			}
+		} else {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	} else {
+		usage()
+		os.Exit(1)
 	}
 }
